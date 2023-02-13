701bd10bc268728d29ac7647f1bd81dfa7cc231a
Invito Digital <fernanda.murillo@uabc.edu.mx>
2023-02-13 12:59:26 -0800

    Assignment 1
    User registration system
    
    - Create register.html with a form with the next fields:  email, password, first name, last name, age, address, phone number, payment, color
    
    - Create the register function using jQuery.
    
    - Get the values from the capture form
    
    - Create an User object using Object Constructor technique and the values from the capture form
    
    - Console log the user object
    
    - --Clear the form after the registration--

--- scripts/register.js
+++ scripts/register.js
@@ -12,6 +12,7 @@ function User(email, pass, fname, lname, age, gender, phone, payment, color){
 }
 
 function register(){
+    //Get the values from the form.
     let inputEmail = $("#txtEmail").val();
     let inputPasswords = $("#txtPassword").val();
     let inputFname = $("#txtFirstName").val();
@@ -22,7 +23,19 @@ function register(){
     let inputPayment = $("#txtPayment").val();
     let inputColor = $("#txtColor").val();
 
+    //Create an newUser object
     let newUser = new User (inputEmail, inputPasswords, inputFname, inputLname, inputAge, inputGender, inputPhone, inputPayment, inputColor);
+
+    //Clear the form
+    $("#txtEmail").val('');
+    $("#txtPassword").val('');
+    $("#txtFirstName").val('');
+    $("#txtLastName").val('');
+    $("#txtAge").val('');
+    $("#txtGender").val('');
+    $("#txtTel").val('');
+    $("#txtPayment").val('');
+    $("#txtColor").val('');
 
     console.log(newUser);
 }
